cec95dfb6736eb41cdb5946671b330c25ced54f4
Skeyling1 <skeyling1@gmail.com>
2024-10-26 15:40:36 +0300

    ddd

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,21 @@
+# написать spider для нахождения всех источников освещения с сайта divan.ru
+# # Нужно взять название источника освещения, цену и ссылку
+# # *Можно попробовать сделать это на другом сайте с продажей источников освещения
+
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lamps = response.css('div.WdR1o')
+        for lamp in lamps:
+
+            yield {
+                'name': lamp.css('div.wYUX2 span::text').get(),
+                'price': lamp.css('div.pY3d2 span::text').get(),
+                'url': lamp.css('a').attrib['href']
+            }
